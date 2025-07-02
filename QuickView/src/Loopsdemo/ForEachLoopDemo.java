1736e04cca8b07566f43ce1724b00c868dbfe5eb
romaa <romaa@ROG-STRIX-SCAR-III>
2025-07-02 20:37:45 +0530

    Quick Review

---
+++ QuickView/src/Loopsdemo/ForEachLoopDemo.java
@@ -0,0 +1,15 @@
+package Loopsdemo;
+
+public class ForEachLoopDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {10,20,30,40,50};
+		
+		//for integer array
+		for (int i:a)
+		{
+			System.out.print(i+ " ");
+		}
+	}
+}
---
+++ QuickView/src/Loopsdemo/ForLoopsDemo.java
@@ -0,0 +1,12 @@
+package Loopsdemo;
+
+public class ForLoopsDemo {
+	public static void main(String[] args) {
+
+		for(int i=1; i<=100 ; i++)
+	
+		{
+			System.out.print("value of i :"+ i + "  ");
+		}
+	}
+}
