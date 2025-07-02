4c522cb059b9e364c01e21de814bcbfddce443c5
romaa <romaa@ROG-STRIX-SCAR-III>
2025-07-02 20:58:40 +0530

    Important Basics

---
+++ Important Basics/src/IdentifierDemo.java
@@ -0,0 +1,27 @@
+
+public class IdentifierDemo {
+	
+	public static void main(String[] args) {
+	//variables declaration - invalid identifier examples 
+	
+			// integer for=13;  error because keyword cannot be be declare as variables 
+			
+			//integer number 1= 13;  error because of space 
+			
+			//integer @number =13; special are not allowed only $ _ ARE ALLOWED 
+			
+			
+			//valid identification
+			
+			
+			int $num1= 20;
+			
+			String StudentName="Heti";
+			
+			//int FOR =12;
+			
+			System.out.println("Values of the number variables is "+ $num1);
+			
+			System.out.println("Student Name is : "+ StudentName);
+	}
+}
