9f4875d60f2788806e402f4840311da9cffc4e88
romaa <romaa@ROG-STRIX-SCAR-III>
2025-07-02 21:57:47 +0530

    Encapsulation

---
+++ Encapsulation/src/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OopsConceptDemo obj=new OopsConceptDemo();
+		
+		obj.setName("Romaan");
+		obj.setAge(21);
+		obj.setSerailNum(1920);
+		
+		System.out.println(obj);
+	}
+
+}
