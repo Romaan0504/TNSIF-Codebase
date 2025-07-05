0181fa66cb5b4525e8ebed937c2456d03ded7dc4
romaa <romaa@ROG-STRIX-SCAR-III>
2025-07-05 11:10:36 +0530

    Final And Static

---
+++ Variable/src/Static_Variable/EmployeeDemo.java
@@ -0,0 +1,14 @@
+package Static_Variable;
+
+public class EmployeeDemo {
+
+public static void main(String[] args) {
+		
+		
+		//Creating first object of the class 
+		
+		Employee e=new Employee("Romaan", 1001);
+		System.out.println(e);
+
+	}
+}
---
+++ Variable/src/Static_Variable/MyClass.java
@@ -0,0 +1,42 @@
+package Static_Variable;
+
+public class MyClass {
+private int section;   //non static variable or instance variable
+	
+	private static int srNo; // static or class variable 
+	
+	
+	//static block
+	static 
+	{
+		System.out.println("------Within Static block-----");
+		srNo=1000;
+	//	section=101;
+	}
+	//Default constructor 
+	public MyClass() {
+		
+	System.out.println("----Within DFefault constructor ----");
+	srNo++;
+	section++;
+	}
+	@Override
+	public String toString() {
+		return "MyClass [serial No "+srNo+",section=" + section + "]";
+	}
+	
+	//static method 
+	static void display()
+	{
+		//System.out.println("Section :"+ section);
+		
+		System.out.println("Serial no :"+srNo);
+		
+		
+	}
+	
+	
+	
+	
+
+}
