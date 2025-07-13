d22bae19ca779eb3edd67b730d574ca0ede39e69
romaa <romaa@ROG-STRIX-SCAR-III>
2025-07-13 23:41:10 +0530

    BANK PROJECT

---
+++ Project/src/DepositLimitException.java
@@ -0,0 +1,12 @@
+@SuppressWarnings("serial")
+public class DepositLimitException extends Exception {
+
+	public DepositLimitException()
+	{
+		super("Daily Limit of deposit is exceed.....");
+	}
+	public DepositLimitException(String message )
+	{
+		super (message);
+	}
+}
